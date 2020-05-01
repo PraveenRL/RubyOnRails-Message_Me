6a45b9079d286cd527ee42bbe92fb86b8c17560c
PraveenRL <rlppraveen@gmail.com>
2020-05-01 10:30:46 +0530

    Install Semantic UI and setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,13 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require rails-ujs
+//= require jquery
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function () {
+    $('.ui.dropdown').dropdown();
+})
